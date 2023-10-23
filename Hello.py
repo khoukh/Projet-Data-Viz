68bc83cb7f3b0c3c9b192b78c3a8508f4b0b143c
khoukh <58174399+khoukh@users.noreply.github.com>
2023-10-23 21:33:17 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,54 +1,97 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-import pandas as pd
 import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+from plotly.subplots import make_subplots
+import plotly.express as px
+import seaborn as sns
 
-st.title('Uber pickups in NYC')
+st.title('Projet Data Visualisation')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+# URL de l'ensemble de données brutes GitHub
+github_url = "https://github.com/tidyverse/ggplot2/raw/main/data-raw/diamonds.csv"
 
+# Charger l'ensemble de données directement à partir de l'URL
 @st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+def load_data():
+    return pd.read_csv(github_url)
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
+# Chargement des données
+df = load_data()
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+# Affichaer les cinq premières lignes du DataFrame 'df' pour obtenir un aperçu initial des données
+st.write("Aperçu des 5 premières lignes du dataset :")
+st.write(df.head())
 
 
-if __name__ == "__main__":
-    run()
+# Définir le style du graphique sur "white" et la palette de couleurs sur "plasma"
+sns.set(style="white")
+sns.set_palette("plasma")
+
+# Titre et description
+st.title("Prix du diamant par carat avec filtre de qualité de taille")
+st.write("Explorer la relation entre le prix du diamant, le carat et la qualité de taille.")
+
+# Options de filtrage
+cut_options = st.multiselect("Select one or more cut types:", df['cut'].unique())
+
+# Filtrer la DataFrame sur la base des coupes sélectionnées
+filtered_df = df[df['cut'].isin(cut_options)]
+
+# Créer un nuage de points
+fig = px.scatter(filtered_df, x="carat", y="price", color="cut", title="Price vs Carat by Cut",color_discrete_sequence=px.colors.sequential.Plasma)
+
+# Personnaliser la mise en page
+fig.update_layout(xaxis_title="Carat", yaxis_title="Price")
+
+# Afficher le nuage de points
+st.plotly_chart(fig)
+
+# Titre et description
+st.title("Nuage de points 3D des diamants")
+st.write("Explorez la relation entre les dimensions des diamants (x, y, z) avec un filtre de qualité de taille.")
+
+# Options de filtrage
+cut_options = st.multiselect("Select one or more cut types:", df['cut'].unique(), key="cut_filter")
+
+# Filtrer le DataFrame sur la base des coupes sélectionnées
+filtered_df = df[df['cut'].isin(cut_options)]
+
+# Créer un nuage de points en 3D
+fig = px.scatter_3d(filtered_df, x="x", y="y", z="z", color="cut", title="3D Scatter Plot of Diamonds",color_discrete_sequence=px.colors.sequential.Plasma)
+
+# Personnaliser la mise en page
+fig.update_layout(scene=dict(xaxis_title="x", yaxis_title="y", zaxis_title="z"))
+
+# Afficher le nuage de points en 3D
+st.plotly_chart(fig)
+
+
+st.title("Diagramme à barres empilées : Qualité de taille par Clarté avec filtre sur les carats")
+
+# Filtre Carat
+carat_min = st.slider("Minimum Carat", min_value=df["carat"].min(), max_value=df["carat"].max(), value=df["carat"].min())
+carat_max = st.slider("Maximum Carat", min_value=df["carat"].min(), max_value=df["carat"].max(), value=df["carat"].max())
+
+# Filtrer le DataFrame sur la base de la gamme de carats sélectionnée
+filtered_df = df[(df["carat"] >= carat_min) & (df["carat"] <= carat_max)]
+
+# Créer un diagramme à barres empilées
+fig = px.bar(
+    filtered_df,
+    x="cut",
+    color="clarity",
+    labels={"cut": "Cut"},
+    color_discrete_sequence=px.colors.sequential.Plasma,
+)
+
+# Personnaliser la mise en page
+fig.update_xaxes(title_text="Cut")
+fig.update_yaxes(title_text="Count")
+fig.update_layout(barmode="stack")
+
+# Afficher le diagramme à barres empilées
+st.plotly_chart(fig)
+
+
